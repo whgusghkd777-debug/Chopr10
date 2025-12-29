b76bdea190834629099a086eb97f53e55db7212b
whgusghkd777-debug <whgusghkd777@gmail.com>
2025-12-29 20:47:55 +0900

    6

--- Chopr11-main/MUSICCHO-main/src/main/java/com/mysite/sbb/music/MusicController.java
+++ Chopr11-main/MUSICCHO-main/src/main/java/com/mysite/sbb/music/MusicController.java
@@ -1,21 +1,5 @@
-package com.mysite.sbb.music;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/music")
-@RequiredArgsConstructor
-public class MusicController {
-
-    private final MusicService musicService;
-
-    @GetMapping("/list")
-    public String list(Model model) {
-        model.addAttribute("musicList", musicService.getList());
-        return "music/list";  // ← 여기 꼭 "music/list" (templates/music/list.html 찾음)
-    }
+@GetMapping("/list")
+public String list(Model model) {
+    model.addAttribute("content", "music/list :: content");
+    return "layout";
 }
